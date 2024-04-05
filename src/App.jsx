d6c7be59bdf1bc20f69faefaa824e91f03979cfd
Harry MacCode <emarh.harry.code@gmail.com>
2024-04-05 12:09:31 +0200

    [Hook] useRef With Input Element

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@ import { useRef } from "react";
 
 
 const App = () => {
-  let myImage = useRef();
+  let firstName = useRef();
+  let lastName = useRef();
   
   const change = () => {
-    myImage.current.src= "https://placehold.co/600x400/000/FFF";
-    myImage.current.setAttribute('height', '300px');
-    myImage.current.setAttribute('width', '300px');
+    let fName = firstName.current.value;
+    let lName = lastName.current.value;
+
+    alert(`Bonjour ${fName} ${lName}`);
   }
   return (
     <div>
       {/* <h1 ref={myHeadLine}></h1> */}
-      <img ref={myImage} src="https://placehold.co/600x400" alt="image" />
+      {/* <img ref={myImage} src="https://placehold.co/600x400" alt="image" /> */}
+      <input ref={firstName} type="text" placeholder="First Name"/><br />
+      <input ref={lastName} type="text" placeholder="Last Name"/><br />
       <button onClick={change}>Click on me for change</button>
     </div>
   )
