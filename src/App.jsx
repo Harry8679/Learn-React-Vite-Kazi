cc14f48f2c0050273c1f71afed3fa9a053d5a5ee
Harry MacCode <emarh.harry.code@gmail.com>
2024-04-05 11:53:13 +0200

    Hooks : useRef3 change an image and his references

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { useRef } from "react";
 
 
 const App = () => {
-  let myHeadLine = useRef();
+  let myImage = useRef();
   
   const change = () => {
-    myHeadLine.current.innerHTML= "<ul><li>Harry</li><li>MacCode</li></ul>";
+    myImage.current.src= "https://placehold.co/600x400/000/FFF";
+    myImage.current.setAttribute('height', '300px');
+    myImage.current.setAttribute('width', '300px');
   }
   return (
     <div>
-      <h1 ref={myHeadLine}></h1>
+      {/* <h1 ref={myHeadLine}></h1> */}
+      <img ref={myImage} src="https://placehold.co/600x400" alt="image" />
       <button onClick={change}>Click on me for change</button>
     </div>
   )
